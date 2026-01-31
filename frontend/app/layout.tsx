fb5f1f560d105601568685939aa9a59a076dc8a9
Romeo Cavazza <romeo.cavazza@epitech.eu>
2026-02-01 00:44:11 +0100

    fix: application configuration florian - structure background et @layer base

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,8 +9,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="min-h-screen bg-[url('/background.png')] bg-cover bg-center bg-no-repeat [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-thumb]:bg-cyberCyan [&::-webkit-scrollbar-thumb]:rounded-sm [&::-webkit-scrollbar-track]:bg-black/20">
-        <div className="min-h-screen bg-black/50">{children}</div>
+      <body className="antialiased">
+        <div className="relative w-screen h-screen">
+          {/* BACKGROUND COMPONENT */}
+          <div 
+            className="fixed inset-0 bg-[url('/background.png')] bg-cover bg-center bg-no-repeat brightness-[0.7] contrast-[1.1] z-0" 
+          />
+          {/* CONTENT OVER BACKGROUND */}
+          <div className="relative z-10">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
