464432cc452dd92bd3778476d35a65bb2f05f0ff
florian-billon <154011027+florian-billon@users.noreply.github.com>
2026-02-09 18:08:09 +0100

    Update config.ts

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -2,5 +2,5 @@
  * Configuration centralisée de l'application
  */
 
-export const API_URL = process.env.API_URL || "http://localhost:3001";
-
+// Utilisation du préfixe NEXT_PUBLIC pour que Vercel puisse transmettre la variable au navigateur
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
